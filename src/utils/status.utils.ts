60ec39b65b903861e3376667e990e0a1d3424078
cakezero <sareahj.com@gmail.com>
2026-01-11 09:30:48 +0100

    status code utils

---
+++ src/utils/status.utils.ts
@@ -0,0 +1,11 @@
+import httpStatus from "http-status";
+
+export const OK = httpStatus.OK;
+
+export const CREATED = httpStatus.CREATED;
+
+export const BAD_REQUEST = httpStatus.BAD_REQUEST;
+
+export const INTERNAL_SERVER_ERROR = httpStatus.INTERNAL_SERVER_ERROR;
+
+export const NOT_FOUND = httpStatus.NOT_FOUND;
