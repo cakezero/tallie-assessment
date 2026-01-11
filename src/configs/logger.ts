3eab987e4ee34b99830ac28121a6b6adb59d59e3
cakezero <sareahj.com@gmail.com>
2026-01-11 09:24:15 +0100

    winston logger setup

---
+++ src/configs/logger.ts
@@ -0,0 +1,28 @@
+import winston from "winston";
+import { SERVER_ENV } from "@/utils/env.utils";
+
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
+const logger = winston.createLogger({
+  level: SERVER_ENV.ENVIRONMENT === "development" ? "debug" : "info",
+  format: winston.format.combine(
+    enumerateErrorFormat(),
+    SERVER_ENV.ENVIRONMENT === "development"
+      ? winston.format.colorize()
+      : winston.format.uncolorize(),
+    winston.format.splat(),
+    winston.format.printf(({ level, message }) => `${level}: ${message}`)
+  ),
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ["error"],
+    }),
+  ],
+});
+
+export default logger;
