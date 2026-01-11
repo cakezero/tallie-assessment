0e4301f41718f69ace9f602725901c4b7642a440
cakezero <sareahj.com@gmail.com>
2026-01-11 09:22:39 +0100

    moved express app setup to seperate file

---
+++ app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import routes from "@/routes";
+
+const app = express();
+
+// app.use(cors({ orgins: SERVER_ENV.ALLOWED_ORIGINS }));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", routes);
+
+export default app;
--- server.ts
+++ server.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { SERVER_ENV } from "./src/utils/env.utils";
+import logger from "@/configs/logger";
+
+const port = SERVER_ENV.PORT;
+
+import app from "./app";
+
+app.listen(port, () => {
+  logger.info(`Server is running on port ${port}`);
+});
